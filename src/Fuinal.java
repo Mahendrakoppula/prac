35b68b4ad689e54ad96c5a8679b26df55b41344d
mahe0309 <mahendrakoppula09@gmail.com>
2022-08-21 20:46:05 +0530

    added new java file fuinal

---
+++ src/Fuinal.java
@@ -0,0 +1,18 @@
+class Demo{
+    void demo(){
+        System.out.println("emo class");
+    }
+}
+class Test extends Demo{
+
+
+
+
+}
+public class Fuinal {
+    public static void main(String[] args){
+        final int i=10;
+        System.out.println(i);
+
+    }
+}
